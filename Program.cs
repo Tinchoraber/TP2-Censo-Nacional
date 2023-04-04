1cfc96627fb42e8628909138d981f89f04abefbb
MARTIN EZEQUIEL RABER <47436029@ort>
2023-04-04 09:00:20 -0300

    Tp 2 terminado

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 Dictionary<int, Persona> DicPersonas = new Dictionary<int, Persona>();
- int numero;
+int numero, dniOtro;
 do
 {
     Console.WriteLine("1.Cargar Alumno");
@@ -9,53 +9,59 @@ do
     Console.WriteLine("4.Modificar Mail de una Persona.");
     Console.WriteLine("5. Salir");
     numero = ingresarNumero("Ingrese un numero:");
-    switch(numero)
+    switch (numero)
     {
         case 1:
-        cargarNuevaPersona();
-        break;
+            cargarNuevaPersona();
+            break;
         case 2:
-        obtenerEstadisticasCenso();
-        break;
+            obtenerEstadisticasCenso();
+            break;
+        case 3:
+            buscarPersona();
+            break;
+        case 4:
+            modificarMailPersona();
+            break;
     }
-} while (numero!=5);
+} while (numero != 5);
 
 int ingresarNumero(string msj)
 {
- int devolver;
- Console.WriteLine(msj);
- devolver = int.Parse(Console.ReadLine());
- while(devolver < 1 && devolver > 5)
- {
-    Console.WriteLine("Ingresaste mal los numeros, volve a hacerlo");
+    int devolver;
     Console.WriteLine(msj);
     devolver = int.Parse(Console.ReadLine());
- }
- return devolver;
+    while (devolver < 1 && devolver > 5)
+    {
+        Console.WriteLine("Ingresaste mal los numeros, volve a hacerlo");
+        Console.WriteLine(msj);
+        devolver = int.Parse(Console.ReadLine());
+    }
+    return devolver;
 }
 void cargarNuevaPersona()
 {
- int dni = ingresarDni();
- string apellido = ingresarTexto("Ingrese su apellido");
- string nombre = ingresarTexto("Ingrese su nombre");
- string email = ingresarTexto("Ingrese su email");
- DateTime fechaNacimiento = ingresarFecha();
- Persona unaPersona = new Persona(dni, apellido, nombre, fechaNacimiento, email);
- DicPersonas.Add(dni, unaPersona);
- Console.WriteLine("Se ha cargado la persona " + nombre + " " + apellido + " al diccionario de Personas");
+    int dni = ingresarDni();
+    string apellido = ingresarTexto("Ingrese su apellido");
+    string nombre = ingresarTexto("Ingrese su nombre");
+    string email = ingresarTexto("Ingrese su email");
+    DateTime fechaNacimiento = ingresarFecha();
+    Persona unaPersona = new Persona(dni, apellido, nombre, fechaNacimiento, email);
+    DicPersonas.Add(dni, unaPersona);
+    Console.WriteLine("Se ha cargado la persona " + nombre + " " + apellido + " al diccionario de Personas");
 }
 int ingresarDni()
-{ 
- int dni;
- Console.WriteLine("Ingrese su DNI");
- dni = int.Parse(Console.ReadLine());
- while(dni < 0 && DicPersonas.ContainsKey(dni))
- {
-    Console.WriteLine("Ingresaste mal los datos, volve a hacerlo");
+{
+    int dni;
     Console.WriteLine("Ingrese su DNI");
     dni = int.Parse(Console.ReadLine());
- }
- return dni;
+    while (dni < 0 && DicPersonas.ContainsKey(dni))
+    {
+        Console.WriteLine("Ingresaste mal los datos, volve a hacerlo");
+        Console.WriteLine("Ingrese su DNI");
+        dni = int.Parse(Console.ReadLine());
+    }
+    return dni;
 }
 string ingresarTexto(string msj)
 {
@@ -70,7 +76,7 @@ DateTime ingresarFecha()
     Console.WriteLine("Ingrese su fecha de nacimiento(aaaa/mm/dd)");
     fechaString = Console.ReadLine();
     DateTime fecha;
-    while(!DateTime.TryParse(fechaString, out fecha))
+    while (!DateTime.TryParse(fechaString, out fecha))
     {
         Console.WriteLine("Ingresaste mal los datos, volve a hacerlo");
         Console.WriteLine("Ingrese su fecha de nacimiento");
@@ -80,26 +86,73 @@ DateTime ingresarFecha()
 }
 void obtenerEstadisticasCenso()
 {
-int cont = 0, acum = 0;
- foreach (Persona item in DicPersonas.Values)
- {
-    if(DicPersonas.Count == 0)
+    int cont = 0, acum = 0;
+    foreach (Persona item in DicPersonas.Values)
+    {
+        acum = acum + item.obtenerEdad();
+        if (item.puedeVotar() == true)
+        {
+            cont++;
+
+        }
+    }
+    if (DicPersonas.Count == 0)
     {
         Console.WriteLine("No hay personas cargadas en la lista");
     }
-    else
-    {
-        Console.WriteLine("Cantidad de personas: " + DicPersonas.Count);
-    }
-    acum = acum + item.obtenerEdad();
-    int promedio = acum / DicPersonas.Count;
-    if(item.obtenerEdad() > 16)
-    {
-        cont++;
-    }
+    Console.WriteLine("Cantidad de personas: " + DicPersonas.Count);
     Console.WriteLine("Cantidad de personas habilitadas para votar: " + cont);
-    Console.WriteLine("Promedio de edad: " + promedio);
- }
-}
+    Console.WriteLine("Promedio de edad: " + acum / DicPersonas.Count);
 
+}
+void buscarPersona()
+{
+    dniOtro = ingresarOtroDni();
+    foreach (int item in DicPersonas.Keys)
+    {
+        if (item == dniOtro)
+        {
+            Console.WriteLine("El nombre de la persona es: " + DicPersonas[item].Nombre);
+            Console.WriteLine("El apellido de la persona es: " + DicPersonas[item].Apellido);
+            Console.WriteLine("La fecha de nacimiento de la persona es: " + DicPersonas[item].FechaNacimiento);
+            Console.WriteLine("El email de la persona es: " + DicPersonas[item].Email);
+            Console.WriteLine("La edad de la persona es: " + DicPersonas[item].obtenerEdad());
+            if (DicPersonas[item].puedeVotar() == true)
+            {
+                Console.WriteLine("Puede votar");
+            }
+            else
+            {
+                Console.WriteLine("No puede votar");
+            }
+        }
+    }
+}
+int ingresarOtroDni()
+{
+    int devolver;
+    Console.WriteLine("Ingrese otro DNI para buscarlo en la lista");
+    devolver = int.Parse(Console.ReadLine());
+    while (devolver < 0)
+    {
+        Console.WriteLine("Ese DNI no es valido");
+        Console.WriteLine("Ingrese otro DNI para buscarlo en la lista");
+        devolver = int.Parse(Console.ReadLine());
+    }
+    if (!DicPersonas.ContainsKey(devolver))
+    {
+        Console.WriteLine("No se encuentra el DNI");
+    }
+    return devolver;
+}
+void modificarMailPersona()
+{
+    int dni3 = ingresarOtroDni();
+    string email2 = ingresarTexto("Ingrese otro mail para actualizarlo");
+    if (DicPersonas.ContainsKey(dni3))
+    {
+        DicPersonas[dni3].Email = email2;
+        Console.WriteLine("El nuevo email de la persona es: " + DicPersonas[dni3].Email);
+    }
+}
 
